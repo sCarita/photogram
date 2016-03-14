794584c76860e481b2cdad5988e8bf622cc17c82
solid <solid@Gustavos-MacBook-Pro.local>
2016-03-14 01:24:31 +0000

    Added delete feature.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,6 +26,12 @@ class PostsController < ApplicationController
     redirect_to post_path(@post)
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
   private
   # Use strong_parameters for attribute whitelisting
   # Be sure to update your create() and update() controller methods.
