4993564cb75f10ee2374ea8a2cf38a714d6af1f8
solid <solid@Gustavos-MacBook-Pro.local>
2016-03-15 03:16:42 +0000

    Added responsive delete of comments. Added new validations to post. Added time_ago at every post.

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,8 @@ class Post < ActiveRecord::Base
   has_attached_file :image, styles: { :medium => "640x" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
+  validates :caption, presence: true, length: { minimum: 3, maximum: 300 }
+
   belongs_to :user
   has_many :comments, dependent: :destroy
 end
