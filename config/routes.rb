da6e0afe5d3a981bf4f86f041d6de94d98ea6038
solid <solid@Gustavos-MacBook-Pro.local>
2016-03-20 21:36:11 +0000

    Added profiles controller. Added routes to profile pages using user_name instead of user_id.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  get ':user_name', to: 'profiles#show', as: :profile
 
   devise_for :users, :controllers => { registrations: 'registrations' }
   root 'posts#index'
